b1a2cece4e79f16fb75ba5e87bcf6a2078af4a16
ropolexi <ropolexi@protonmail.com>
2021-10-01 23:19:29 +0530

    the first commit

---
+++ DeSoLib.h
@@ -0,0 +1,81 @@
+#ifndef DeSoLib_h
+#define DeSoLib_h
+
+#include <HTTPClient.h>
+#include "WiFi.h"
+#include <stdlib.h>
+
+#define DEBUG_LOG false
+
+#define debug_print(...) \
+            do { if (DEBUG_LOG) Serial.print(__VA_ARGS__); } while (0)
+class DeSoLib{
+    public:
+    WiFiClientSecure espClientSecure;
+    DeSoLib();
+    const char *RoutePathHealthCheck = "/api/v0/health-check";
+    const char *ExchangeRateRoute = "/api/v0/get-exchange-rate";
+    const char *RoutePathGetSingleProfile = "/api/v0/get-single-profile";
+    const char *RoutePathGetUsersStateless = "/api/v0/get-users-stateless";
+    const char *RoutePathGetHodlersForPublicKey = "/api/v0/get-hodlers-for-public-key";
+    const char *RoutePathGetPostsForPublicKey = "/api/v0/get-posts-for-public-key";
+    const char *RoutePathGetBalance = "/api/v1/balance";
+
+    struct Node{
+        char url[50];
+        bool status=false;
+        const char* caRootCert;
+
+    };
+    struct Profile{
+        char PublicKeyBase58Check[56];
+        char Username[20];
+        double CoinsInCirculationNanos;
+        double CoinPriceBitCloutNanos;
+        double BalanceNanos;
+        double UnconfirmedBalanceNanos;
+        double TotalHODLBalanceClout;
+        int TotalHodleNum;
+        char TopHodlersUserNames[10][20];
+        int lastPostLikes;
+        int lastPostDiamonds;
+
+    };
+
+    char buff_small_1[200];
+    char buff_small_2[200];
+    char buff_large[1024];//heavy usage on web response
+
+    double USDCentsPerBitCloutExchangeRate;
+    double USDCentsPerBitcoinExchangeRate;
+   
+    int selectedNodeIndex=0;
+    void addNodePath(const char* url,const char* cert);
+    int getMaxNodes();
+    void selectDefaultNode(int index);
+    char* getSelectedNodeUrl();
+    bool getSelectedNodeStatus();
+    const char* getRequest(const char* apiPath);
+    const char * postRequest(const char* apiPath,const char *data);
+    const char* getNodeHealthCheck();
+    int updateNodeHealthCheck();
+    const char* getExchangeRates();
+    int updateExchangeRates();
+    const char * getSingleProfile(const char *messagePayload);
+    int updateSingleProfile(const char *username,const char *PublicKeyBase58Check,Profile *prof);
+    const char * getUsersStateless(const char *messagePayload);
+    int updateUsersStateless(const char *PublicKeysBase58Check,bool SkipHodlings,Profile *prof);
+    const char *getHodlersForPublicKey(const char *messagePayload);
+    int updateHodlersForPublicKey(const char *username,const char *PublicKeyBase58Check,int NumToFetch,Profile *prof);
+    void clearTopHodlersUserNames(Profile *prof);
+    const char *getPostsForPublicKey(const char *messagePayload);
+    int updateLastPostForPublicKey(const char *PublicKeysBase58Check,Profile *prof);
+    const char * getUserBalance(const char *messagePayload);
+    int updateUsersBalance(const char *PublicKeysBase58Check,Profile *prof);
+    ~DeSoLib();
+
+    private:
+    std::vector<Node> nodePaths;
+
+};
+#endif
---
+++ README.md
@@ -0,0 +1,85 @@
+# DeSo Arduino Library
+
+## Introduction
+This is an unofficial personal arduino library for the DeSo (Decentralized Social) Blockchain.
+
+"DeSo is a new layer-1 blockchain built from the ground up to scale decentralized social applications to one billion users" [https://www.deso.org]
+
+## Features
+- Multiple DeSo Nodes supported for decentralization 
+- DeSo Coin Value
+- Creator Coin Price using username or PublicKey
+- Wallet Balance (V0 API)
+- All HODLE Asset balance
+- Wallet Balance
+- All HODLE Asset balance
+- Top 10 Hodlers
+- Last post like count
+- Last post diamond count
+- V1 API to get balance
+
+Note: Top 10 Hodlers (Recommand Top 5 in case memory issue)
+
+## Serial Output Results
+```
+DeSo Node: https://bitclout.com
+Node Status: Synced OK
+DeSo Coin Value: $121.90
+=======Profile========
+Username: ropolexi
+PublicKey: BC1YLfghVqEg2igrpA36eS87pPEGiZ65iXYb8BosKGGHz7JWNF3s2H8
+Creator Coin Price: $31.68
+Wallet Balance: $1.02
+Total HODLE assets : 4
+Total HODLE Asset Balance: $35.40
+======================
+
+DeSo Node: https://nachoaverage.com
+Node Status: Synced OK
+DeSo Coin Value: $121.90
+=======Profile========
+Username: ropolexi
+PublicKey: BC1YLfghVqEg2igrpA36eS87pPEGiZ65iXYb8BosKGGHz7JWNF3s2H8
+Creator Coin Price: $31.68
+Wallet Balance: $1.02
+Total HODLE assets : 4
+Total HODLE Asset Balance: $35.40
+======================
+
+DeSo Node: https://members.giftclout.com
+Node Status: Synced OK
+DeSo Coin Value: $121.90
+=======Profile========
+Username: ropolexi
+PublicKey: BC1YLfghVqEg2igrpA36eS87pPEGiZ65iXYb8BosKGGHz7JWNF3s2H8
+Creator Coin Price: $31.68
+Wallet Balance: $1.02
+Total HODLE assets : 4
+Total HODLE Asset Balance: $35.40
+======================
+```
+## Device Supported
+
+ESP32 Module (US $5.99)
+
+https://www.ebay.com/itm/312621586322?hash=item48c9b2d792:g:BtUAAOSwYMlc4sAF
+
+
+## Dependency Libraries
+ArduinoJson - https://github.com/bblanchon/ArduinoJson
+
+## Limitations
+Tested an account with the following parameters
+- following 500
+- assets (Hodle) 13
+
+No low memory issues. 
+Always try to do the project on a fresh account to get optimum results for the end user device such as a vending machine.
+
+But if the number of following is huge (more than 600) and number of assets are huge number(greater than 50) 
+account balance or all the assets hodle balance will not be shown due to memory limitation.
+
+**This library does not need any seed phrase to access any accounts**
+
+**Use this library at your own risk, I will not take any responsible for any damages using this library**
+
